6a1b5e9d0a7b950952b02b3176e78ed1753c710d
Nezarik-Intmax <nezarikintmax@gmail.com>
2021-11-06 20:47:51 +0500

    Добавьте файлы проекта.

---
+++ OOP_Lab3/OOP_Lab3.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+template <class T>
+class Container{
+private:
+    T* next;
+    T* prev;
+public:
+    Container(){};
+    void pop(T a);
+    void push(T a);
+};
+void Container:pop(T a){
+
+}
+void Container:push(T a){
+
+}
+
+int main(){
+    Container<int> a;
+}
